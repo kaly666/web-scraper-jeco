cca25d7bcc659505851f14606f7d8ba3e2ede1b2
Kaly <71524855+kaly666@users.noreply.github.com>
2023-10-04 03:31:03 +0300

    enchanced api functionality (extras)

--- src/server/flows/getPosts.js
+++ src/server/flows/getPosts.js
@@ -1,53 +1,71 @@
 // const puppeteer = require('puppeteer');
 const puppeteerInstance = require('../services/puppeteer');
-const getRemFontSize = require('../utils/getRemFontSize')
 
 async function getPosts(query) {
     let { url, extras } = query;
     const browser = await puppeteerInstance.instance;
     const page = await browser.newPage();
 
-    await page.goto(url);
+    await page.goto(url, { waitUntil: 'networkidle2' });
 
-    const posts = await page.evaluate((extrasIn) => {
+    const posts = await page.evaluate((extrasQuery) => { 
         let getRemFontSize = (element) => { return parseFloat(getComputedStyle(element).fontSize) / parseFloat(getComputedStyle(document.documentElement).fontSize) }
-        const posts = Array.from(document.querySelectorAll('div:nth-child(2)'));
+        const posts = Array.from(document.querySelectorAll('main > div > div > div > div'));
         var postsArray = [];
         for(let post of posts) { 
-            let title =  Array.from(post.querySelectorAll('div')).find(x => getRemFontSize(x) == 1.125);
-            let description = Array.from(post.querySelectorAll('div')).find(x => getRemFontSize(x) == 0.875);
-            if(!title || !description) continue;
-            let extrasOut = {};
-            if(extrasIn){
-                extrasIn.split(',').forEach(extra => {
-                    if(!extrasOut[extra]) extrasOut[extra] = [];
+            var title =  Array.from(post.querySelectorAll('div')).find(x => getRemFontSize(x) == 1.125);
+            var description = Array.from(post.querySelectorAll('div')).find(x => getRemFontSize(x) == 0.875);
+            var extrasOut = null;
 
-                    let foundElements = Array.from(post.querySelectorAll(extra));
-                    foundElements.forEach(element => {
-                        if(element.href)
-                            extrasOut[extra] = [...extrasOut[extra], element.href]
-                        else if(element.src)
-                            extrasOut[extra] = [...extrasOut[extra], element.src]
-                        else if(element.innerText)
-                            extrasOut[extra] = [...extrasOut[extra], element.innerText]
-                    })
-                })
+            if(!title || !description){
+                // fallback for other websites, why not
+                var title = Array.from(post.querySelectorAll('h3'));
+                var description = Array.from(post.querySelectorAll('p')).length ? Array.from(post.querySelectorAll('p')) : Array.from(post.querySelectorAll('span'));
+                if(!title || !description) continue;
             }
-            if(!postsArray.find(x => x.title == title.innerText))
-                postsArray.push({
-                    id: title.innerText.toLowerCase().replace(/ /g, '-'),
+            
+            if(extrasQuery?.split(',').length){
+                extrasOut = {};
+                for(let extra of extrasQuery.split(',')){
+                    try {
+                        extrasOut[extra] = Array.from(post.querySelectorAll(extra));
+                        if(!extrasOut[extra].length) continue;
+
+                        if(extrasOut[extra][0]?.src) {
+                            extrasOut[extra] = extrasOut[extra]?.map(x => {
+                                return x.src;
+                            })
+                        } else if(extrasOut[extra][0]?.href) {
+                            extrasOut[extra] = extrasOut[extra]?.map(x => {
+                                return x.href
+                            })
+                        } else if(extrasOut[extra][0]?.innerText) {
+                            extrasOut[extra] = extrasOut[extra]?.map(x => {
+                                return x.innerText
+                            })
+                        }
+                    } catch(err) {
+                        console.log(err);
+                    }
+                }
+            }
+
+            if(!postsArray.find(x => x.title == title.innerText) && title.innerText){
+                let queryResult = {
+                    id: title.innerText?.toLowerCase().replace(/ /g, '-'),
                     title: title.innerText,
-                    description: description.innerText,
-                    extras: extrasOut
-                })
+                    description: description.innerText
+                }
+                if(extrasOut) {
+                    queryResult = Object.assign(queryResult, {extras: extrasOut})
+                }
+                postsArray.push(queryResult)
+            }
         }
-        console.log(postsArray)
         return postsArray;
     }, extras)
-
+    console.log(posts);
     
-    await page.close();
-
     return posts;
 }
 
